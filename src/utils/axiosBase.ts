cd330c27e412a8dee30e3fa55e66d9c3443ff166
Hachikoi-the-creator <facka72@gmail.com>
2023-05-16 12:39:39 -0500

    updated axios base

--- src/utils/axiosBase.ts
+++ src/utils/axiosBase.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api";
+const baseURL = "/api";
 
 export const axiosBase = axios.create({ baseURL });
